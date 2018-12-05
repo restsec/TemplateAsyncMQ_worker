d0a3c8e4138fe289ff77d6526f43614cbe5cca2e
auyer <rafael.passos@redes.unb.br>
2018-12-05 15:24:42 -0200

    Added retry and error catching to Writer

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pika as rmq
 import logging
 
+QUEUE_ADDR = 'localhost'
 NAME = 'workername'
 READQUEUE = 'todoq'
 WRITEQUEUE = 'resultq'
+WRITE_RETRY = 3
 
 
 def handler(body):
@@ -19,31 +21,45 @@ logging.basicConfig(format='%(asctime)s,%(msecs)-3d - %(name)-12s - %(levelname)
                         level=logging.INFO)
 logging.info("Inicializando o worker %s" %NAME )
 
-connection = rmq.BlockingConnection(rmq.ConnectionParameters(host='localhost'))
+connection = rmq.BlockingConnection(rmq.ConnectionParameters(host=QUEUE_ADDR))
 channel = connection.channel()
 
 channel.queue_declare(queue=READQUEUE, durable=True)
 logging.info(' [*] Waiting for messages. To exit press CTRL+C')
 
-def writer(message):
-  connection = rmq.BlockingConnection(rmq.ConnectionParameters(host='localhost'))
-  channel = connection.channel()
+def writer(message, retry):
 
-  channel.queue_declare(queue=WRITEQUEUE, durable=True)
+  try:
+    connection = rmq.BlockingConnection(rmq.ConnectionParameters(host=QUEUE_ADDR))
 
-  channel.basic_publish(exchange='',
-                        routing_key=WRITEQUEUE,
-                        body=message,
-                        properties=rmq.BasicProperties(
-                          delivery_mode = 2, # make message persistent
-                        ))
-  logging.info(" [x] Sent %r" % message)
-  connection.close()
+    channel = connection.channel()
+
+    channel.queue_declare(queue=WRITEQUEUE, durable=True)
+
+    channel.basic_publish(exchange='',
+                          routing_key=WRITEQUEUE,
+                          body=message,
+                          properties=rmq.BasicProperties(
+                            delivery_mode = 2, # make message persistent
+                          ))
+    logging.info(" [x] Sent %r" % message)
+    connection.close()
+
+  except Exception as err:
+    if retry <= 0:
+      logging.error('Failed publishing to Queue after %d attempts.' %WRITE_RETRY)
+      logging.critical(err)
+      return
+
+    logging.info('Failed publishing to Queue. Remaining attempts : %d' %retry)
+    writer(message, retry - 1)
+  
+  
 
 def callback(ch, method, properties, body):
   ### TODO Investigar qual a melhor prática neste caso: esperar a writer postar para mandar o ack, ou mandar antes de chamar a writer.
   logging.info(" [x] Received %r" % body)
-  writer(handler(body))
+  writer(handler(body),WRITE_RETRY)
   ch.basic_ack(delivery_tag = method.delivery_tag)
   logging.info(" [x] Done")
 
