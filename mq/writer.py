eb775ecd37db6f4c036b1383a9436eb950ced3ac
auyer <rafael.passos@redes.unb.br>
2019-01-16 13:24:07 -0200

    updated writer to return error

--- mq/writer.py
+++ mq/writer.py
@@ -1,55 +1,57 @@
 import pika as rmq
 import logging
 
+
 # This function 
 def write_message(message, QUEUE_ADDR, WRITE_QUEUE, retry=3):
-  """
-  write_message function will post a message to a queue using the AMQP 0-9-1 protocol.
+    """
+    write_message function will post a message to a queue using the AMQP 0-9-1 protocol.
 
-  This function helps ensure all messages are posted correclty. They are posted with delivery_mode = 2, this means they are made persistent, and if the queue fails, the messages will be kept in disk storage.
+    This function helps ensure all messages are posted correclty. They are posted with delivery_mode = 2, this means they are made persistent, and if the queue fails, the messages will be kept in disk storage.
 
-  Parameters
-  ----------
-  message : str
-      Message to be posted as body in the message queue. It should be encoded in the desired scheme (JSON, MessagePack)
-  QUEUE_ADDR : str
-      Domain name / ip address of the message queue.
-  WRITE_QUEUE : str
-      Queue name. This should be the same name as the one used in the desired consumer.
-  retry : int
-      Numer of retry attempts to be performed if the operation fails.
-      
+    Parameters
+    ----------
+    message : str
+        Message to be posted as body in the message queue. It should be encoded in the desired scheme (JSON, MessagePack)
+    QUEUE_ADDR : str
+        Domain name / ip address of the message queue.
+    WRITE_QUEUE : str
+        Queue name. This should be the same name as the one used in the desired consumer.
+    retry : int
+        Numer of retry attempts to be performed if the operation fails.
 
-  Returns
-  -------
-  bool
-      True : Operation Successfull
-      False: Operation Failed. In this case, logging.error and logging.critical messages will be produced, after n logging.info messages, n being the ammount of attempts.
 
-  """
-  try:
-    connection = rmq.BlockingConnection(rmq.ConnectionParameters(host=QUEUE_ADDR))
+    Returns
+    -------
+    None / Exception
+        None : Operation Successfull
+        Exception: Operation Failed. In this case, logging.error and logging.critical messages will be produced, after n logging.info messages, n being the ammount of attempts.
 
-    channel = connection.channel()
+    """
+    err = None
+    try:
+        connection = rmq.BlockingConnection(rmq.ConnectionParameters(host=QUEUE_ADDR))
 
-    channel.queue_declare(queue=WRITE_QUEUE, durable=True)
+        channel = connection.channel()
 
-    channel.basic_publish(exchange='',
-                          routing_key=WRITE_QUEUE,
-                          body=message,
-                          properties=rmq.BasicProperties(
-                            delivery_mode = 2, # make message persistent
-                          ))
-    logging.info(" [x] Sent %r" % message)
-    connection.close()
+        channel.queue_declare(queue=WRITE_QUEUE, durable=True)
 
-  except Exception as err:
-    if retry <= 0:
-      logging.error('Failed publishing to Queue after all attempts.')
-      logging.critical(err)
-      return False
+        channel.basic_publish(exchange='',
+                              routing_key=WRITE_QUEUE,
+                              body=message,
+                              properties=rmq.BasicProperties(
+                                delivery_mode=2, # make message persistent
+                              ))
+        logging.info(" [x] Sent %r" % message)
+        connection.close()
 
-    logging.info('Failed publishing to Queue. Remaining attempts : %d' %retry)
-    write_message(message, QUEUE_ADDR, WRITE_QUEUE, retry - 1)
-  return True
+    except Exception as err:
+        if retry <= 0:
+            logging.error('Failed publishing to Queue after all attempts.')
+            logging.critical(err)
+            return err
+
+        logging.info('Failed publishing to Queue. Remaining attempts : %d' %retry)
+        write_message(message, QUEUE_ADDR, WRITE_QUEUE, retry - 1)
+    return err
 
